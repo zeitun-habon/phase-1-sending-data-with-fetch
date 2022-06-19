996ab43071ea6918e63f98d5a36bb5c2c1a91776
zeitun-adan <zeitun.adan@student.moringaschool.com>
2022-06-19 23:59:53 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your code here
+function submitData(name , email) {
+    const info = {name, email}
+     return fetch("http://localhost:3000/users",{
+        method:"POST",
+        headers:{
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(info)
+    })
+    .then(response=>response.json()
+      )
+      .then(data=> {
+          document.body.innerHTML = data.id
+      })
+      .catch( error => {document.body.innerHTML = error.message});
+}
